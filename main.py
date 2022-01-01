4baad041b0792ae6a07dc9d7ef71a1b6b1c2df5e
Tam Le <tamhle824@gmail.com>
2021-12-31 20:44:01 -0600

    3nd

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
 #primitive types
 
+def count_bits(x):
+    num_bits = 0
+    while x:
+        num_bits += x & 1
+        x >>= 1 
+    return num_bits
+
+
+
+if __name__ == "__main__":
+    print(count_bits(10))
